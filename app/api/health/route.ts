49005f4737455a0b80eb71ac2301e68e1d48b259
Deodeep kunj <77825051+deodeepkunj@users.noreply.github.com>
2026-01-28 15:29:42 +0530

    health check api added

---
+++ app/api/health/route.ts
@@ -0,0 +1,9 @@
+export async function GET() {
+    return Response.json(
+        {
+            status: 'healthy',
+            timestamp: new Date().toISOString()
+        },
+        {status: 200}
+    );
+}
